46f984ee1e167806cb7206f4c584867864c7339f
JourneyHM <A01662556@tec.mx>
2024-10-13 01:22:14 -0600

    Generación router y pagina de login.

---
+++ asr-front-end/src/routes/PrivateRouter.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+const PrivateRouter = () => {
+
+  return (
+    <>
+        <div >
+            <h1>PrivateRouter</h1>
+        </div>
+    </>
+  );
+};
+
+export default PrivateRouter;
---
+++ asr-front-end/src/routes/PublicRouter.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+const PublicRouter = () => {
+
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+};
+
+export default PublicRouter;
